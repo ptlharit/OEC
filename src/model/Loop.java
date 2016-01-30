3e727603c29c29b9176ddde3fb30abf5aee29a24
nishp763 <nishp763@gmail.com>
2016-01-29 22:32:17 -0500

    Loop.java Done

--- src/model/Loop.java
+++ src/model/Loop.java
@@ -4,7 +4,7 @@ public class Loop
 {
 	String [] story = {"Nish is Eating when he is hungry.","Nish is outside and he is walking."};
 	String [] question = {"Can you make Nish Eat?\nHint - try typing While Nish is Hungry, Nish is Eating","Can you make Nish Walk?"};
-	String [] answer = {"Nish is Hungry, Nish is Eating","While Nish is Outside, Nish is Walking"};
+	String [] answer = {"While Nish is Hungry, Nish is Eating","While Nish is Outside, Nish is Walking"};
 	
 	public String getStory(int module) 
 	{
