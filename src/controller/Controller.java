ab744920514d77d738890e896c75a8377f612c21
Terin Dhadda <terindhadda@hotmail.com>
2016-01-29 23:44:36 -0500

    fixing view and starting controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,6 +4,10 @@ import view.MainScreen;
 
 public class Controller {
 	
+	public static String[] next(int module, int slideNumber) {
+		
+	}
+	
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		MainScreen ms = new MainScreen();
